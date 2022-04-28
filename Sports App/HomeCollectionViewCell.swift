e996f55ce4e4f16dfd2d98e4a3147f63e20db134
MohabElziny <mohabziny@gmail.com>
2022-04-28 13:02:41 +0200

    home screen ui

---
+++ Sports App/HomeCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  HomeCollectionViewCell.swift
+//  Sports App
+//
+//  Created by Mohab El-Ziny on 26/04/2022.
+//
+
+import UIKit
+
+class HomeCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var cellImageView: UIImageView!
+    @IBOutlet weak var cellLabel: UILabel!
+}
