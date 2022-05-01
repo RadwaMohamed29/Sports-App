7877e128efe06ec0090bddfcddbeb2eac0811e03
RadwaMohamed29 <radwamohamed2979@gmail.com>
2022-04-30 21:17:14 -0700

    updating

--- Sports App/LeaguesViewController.swift
+++ Sports App/LeaguesViewController.swift
@@ -8,13 +8,18 @@
 import UIKit
 
 class LeaguesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
-    @IBOutlet weak var tbView: UITableView!
+    
+    @IBOutlet weak var leaguesTableView: UITableView!{
+        didSet{
+            leaguesTableView.dataSource = self
+            leaguesTableView.delegate = self
+            self.leaguesTableView.register(UINib(nibName: "LeagueTableViewCell", bundle: nil), forCellReuseIdentifier: "leagueTableViewCell")
+            
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        tbView.delegate=self
-        tbView.dataSource=self
-        self.tbView.register(UINib(nibName: "LeagueTableViewCell", bundle: nil), forCellReuseIdentifier: "cell")
         
 
         
@@ -27,21 +32,27 @@ class LeaguesViewController: UIViewController, UITableViewDelegate, UITableViewD
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 3
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tbView.dequeueReusableCell(withIdentifier: "cell", for:indexPath) as! LeagueTableViewCell
+        guard  let cell = leaguesTableView.dequeueReusableCell(withIdentifier: "leagueTableViewCell", for:indexPath) as? LeagueTableViewCell   else{fatalError()}
+        setUpCell(with: cell, indexPath: indexPath)
+        
+        return cell
+        
+        
+    }
+    
+    func setUpCell(with cell: LeagueTableViewCell, indexPath: IndexPath){
         cell.leagueImage.layer.borderWidth = 1
         cell.leagueImage.layer.masksToBounds = false
         cell.leagueImage.layer.borderColor = UIColor.black.cgColor
         cell.leagueImage.layer.cornerRadius = cell.leagueImage.frame.height/2
         cell.leagueImage.clipsToBounds = true
-        return cell
+        
     }
     
-
-
 }
